738c3d87876c22a6589c07ac583b4e887256884f
Erica Winberry <elaewin@gmail.com>
2016-10-09 16:22:07 -0700

    Added coding challenge text at the bottom of NewTodoController.swift.

--- CFS3ToDoList/NewTodoViewController.swift
+++ CFS3ToDoList/NewTodoViewController.swift
@@ -55,3 +55,9 @@ class NewTodoViewController: UIViewController, UITextFieldDelegate {
     */
 
 }
+
+//Coding Challenges
+
+//1. There is an issue with the code above. When we create our TodoList and close the app, then relaunch, our TodoList is empty. This is where Persistence would come into play. To implement persistence into your TodoList Application, I would recommend following this tutorial here: Persistence Tutorial
+
+//2. Another issue with our TodoList is that users cannot remove Todo items once they are completed. Look into UITableViewDelegate. There is a method called tableView(_:commitEditingStyle:forRowAt:) that will allow us to implement a "Swipe to Delete". This method will use our TodoList.share.remove() function to remove the Todo for the specific index.
